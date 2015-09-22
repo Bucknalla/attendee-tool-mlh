36a9e936f00b0636a3180d5a5eb343e8b8c550fe
Alex Bucknall <alex.bucknall@gmail.com>
2015-09-22 09:48:27 -0400

    Corrections

--- menu.py
+++ menu.py
@@ -3,7 +3,7 @@ import webbrowser
 import mlh
 
 @rumps.clicked('Attendees','Dietary Restrictions')
-def open_about(self):
+def click_diet(self):
     window = rumps.Window(dimensions=(320,120))
     window.title = 'Dietary Restrictions'
     window.message = 'Information regarding dietary restrictions.'
@@ -12,7 +12,7 @@ def open_about(self):
     window.run()
 
 @rumps.clicked('Attendees','Medical Needs')
-def open_about(self):
+def click_med(self):
     window = rumps.Window(dimensions=(320,120))
     window.title = 'Medical Needs'
     window.message = 'Information regarding medical needs.'
@@ -21,42 +21,50 @@ def open_about(self):
     window.run()
 
 @rumps.clicked('MLH','Website')
-def something(self):
+def click_web(self):
     webbrowser.open_new_tab('http://www.mlh.io')
 
 @rumps.clicked('MLH','Guides','Organiser')
-def something(self):
+def click_org(self):
     webbrowser.open_new_tab('http://guide.mlh.io')
 
 @rumps.clicked('MLH','Guides','Sanctions')
-def something(self):
+def click_sanctions(self):
     webbrowser.open_new_tab('http://static.mlh.io/docs/event-sanctioning-guide.pdf')
 
 @rumps.clicked('MLH','Guides','Code of Conduct')
-def something(self):
+def click_coc(self):
     webbrowser.open_new_tab('http://static.mlh.io/docs/mlh-code-of-conduct.pdf')
 
 @rumps.clicked('MLH','Contacts','Info')
-def something(self):
+def click_info(self):
     webbrowser.open_new_tab('mailto:hi@mlh.io')
 
 @rumps.clicked('MLH','Contacts','Incidents')
-def something(self):
+def click_incidents(self):
     webbrowser.open_new_tab('mailto:incidents@mlh.io')
 
 @rumps.clicked('MLH','Contacts','Slack')
-def something(self):
+def click_slack(self):
     webbrowser.open_new_tab('https://mlh.slack.com/?redir=%2Fhome')
 
 @rumps.clicked('About')
-def open_about(self):
+def click_about(self):
     window = rumps.Window(title="About",message="Created by Alex Bucknall 2015", dimensions=(0,0))
     window.icon = None
     window.run()
 
+@rumps.clicked('Search')
+def click_search(self):
+    response = rumps.Window(cancel='Cancel',title='Enter a name',message='Enter a complete name or search by first/last name.',dimensions=(300,20)).run()
+    if response.clicked:
+        rumps.alert(title='Search: '+response.text,message=mlh.search_user(response.text))
+
+
 if __name__ == "__main__":
     app = rumps.App("MLH Organiser Tool",  icon='mlh.png')
     app.menu = [
+        'Search',
         {'Attendees':
             {"Dietary Restrictions": [],
              "Medical Needs": []}},
--- mlh.py
+++ mlh.py
@@ -26,6 +26,7 @@ def get_dietary_users():
 
 def get_special_users():
     user = fetch_user()
+    user_str = ''
 
     special_user = [item for item in user["data"]
         if item["special_needs"] != None]
@@ -35,3 +36,26 @@ def get_special_users():
         for users in special_user:
             user_str += users['first_name'] + " " + users['last_name'] + " : " + users['special_needs'] +'\n'
         return user_str
+
+def search_user(name):
+    user = fetch_user()
+    user_str = ''
+
+    found_users = [item for item in user["data"]
+        if (item["first_name"] + " " + item["last_name"]) == name or item['first_name'] == name or item['last_name'] == name]
+
+    for user in found_users:
+        user_id = "ID: " + str(user['id']) + '\n'
+        first_name = "First Name: " + user['first_name'] + '\n'
+        last_name = "Last Name: " + user['last_name'] + '\n'
+        phone_number = "Phone Number: " + user['phone_number'] + '\n'
+        email = "Email: " + user['email'] + '\n'
+        gender = "Gender: " + user['gender'] + '\n'
+        major = "Major: " + user['major'] + '\n'
+        date_of_birth = "D.O.B: " + user['date_of_birth'] + '\n'
+        school = "School: " + user['school']['name'] + '\n'
+        diet = "Dietary Restrictions: " + user['dietary_restrictions'] + '\n'
+        medical = "Medical Needs: " + str(user['special_needs']) + '\n'
+        shirt_size = "Shirt Size: " + user['shirt_size'] + '\n'
+        user_str += user_id+first_name+last_name+phone_number+email+gender+major+date_of_birth+school+diet+medical+shirt_size+'\n'
+    return user_str
